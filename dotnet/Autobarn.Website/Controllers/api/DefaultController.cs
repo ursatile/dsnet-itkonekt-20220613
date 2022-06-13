990a5984028fe1481c4428d8fe641263e2f6aa10
dylanbeattie <dylan@dylanbeattie.net>
2022-06-13 10:28:50 +0100

    First working example of hypermedia for API pagination

---
+++ dotnet/Autobarn.Website/Controllers/api/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Autobarn.Website.Controllers.api {
+    [Route("api")]
+    [ApiController]
+    public class DefaultController : ControllerBase {
+        [HttpGet]
+        public IActionResult Get() {
+            var result = new {
+                _links = new {
+                    vehicles = new {
+                        href = "/api/vehicles"
+                    }
+                },
+                message = "Welcome to the Autobarn API",
+                version = Assembly.GetExecutingAssembly().FullName,
+            };
+            return Ok(result);
+        }
+    }
+}
